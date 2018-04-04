057d8e1aeca21deeb66e9b64e01946d8d568a945
wsms <wsms@gft.com>
2018-04-04 18:05:35 -0300

    add projeto do mkyong - gacp2

---
+++ projeto-aula-teste/src/main/java/br/com/impacta/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package br.com.impacta.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
--- projeto-aula-teste/src/main/java/br/com/impacta/config/WebConfig.java
+++ projeto-aula-teste/src/main/java/br/com/impacta/config/WebConfig.java
@@ -2,12 +2,15 @@ package br.com.impacta.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @EnableWebMvc //mvc:annotation-driven
 @Configuration
 @ComponentScan({"br.com.impacta.controller"})
+@Import({ br.com.impacta.config.SecurityConfig.class })
 public class WebConfig extends  WebMvcConfigurerAdapter {
 
 }
