3cc32f4f2e92a5806ce7180514d1789a65329530
wsms <wsms@gft.com>
2018-04-04 16:34:32 -0300

    derivando projetos - gacp2

---
+++ spring-security-annotation/src/main/java/com/williansmartins/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.williansmartins.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
